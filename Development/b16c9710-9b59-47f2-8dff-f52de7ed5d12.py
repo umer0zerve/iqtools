11f7ab3ec8d1ee4aba01c7765520613d4d0f129f
umer0zerve <107664370+umer0zerve@users.noreply.github.com>
2024-05-01 09:09:15 +0530

    Delete Development/b16c9710-9b59-47f2-8dff-f52de7ed5d12.py

--- Development/b16c9710-9b59-47f2-8dff-f52de7ed5d12.py
+++
@@ -1,4 +0,0 @@
-Question = """Generate a 3 week tour itinerary for 3 member family (1 kid) to travel across Italy. 
-Make sure to add offbeat locations in it. Give me daily plan with schedules. 
-If possible add recommendation on where to stay and what to eat in each location.
-Print the itinery as text not"""
